be81de08a1dd5042c524a996fcac7a2f9172378f
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-05-27 21:32:19 +0530

    Add files via upload

---
+++ writing program without library fun.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int stringlength(char [])
+int main()
+{
+	char str[30];
+	unsigned int len;
+	printf("enter string:");
+	gets(str);
+	len=stringlenght(str);
+	prinf("length:%d",len);
+}
+int stringlength(char h[])
+{
+	int i=0,count=0;
+	while(x[1]!='\0')
+	{
+		count++;
+		i++;
+	}
+	return count;
+}
+
+
+
+
+
+
+
