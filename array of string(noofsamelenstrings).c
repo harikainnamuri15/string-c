708297b8ce39e70274b2e2c924ecc59c0cec7dd9
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-05-29 21:56:07 +0530

    Add files via upload

---
+++ array of string(noofsamelenstrings).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100][100];
+	int i,size,ml,maxpos;
+	printf("enter the size of array:");
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%[^\n]s",&str[i]);
+	}
+	maxpos=0;
+	for(i=0;i<size;i++)
+	{
+		if(strlen(str[i])>strlen(str[maxpos]))
+		{
+			maxpos=1;
+		}
+	}
+	ml=strlen(str[maxpos]);
+	for(i=0;i<size;i++)
+	{
+		printf("%d %s %d\n",i,str[i],strlen(str[maxpos]));
+	}
+	return 0;
+}
