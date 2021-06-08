269c156d9c5c8ef64af62aaf0b9b8553c66e79c3
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-06-08 21:40:48 +0530

    Add files via upload

---
+++ string declaration(various forms).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char str[10]={"hello"};
+	printf("%s\n",str);
+	char *ptr="hello";
+	printf("%s\n",ptr);
+	char str1[]={"hello"};
+	printf("%s\n",str);
+	char str2[10]={'h','e','l','l','o'};
+	printf("%s",str);
+}
+
+
